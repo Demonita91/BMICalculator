1a283c198fc69f8f5f255b968f9ebb759aad11fc
asus <de>
2020-09-17 14:04:17 +0300

    Added test checkOverweightCategoryHighLimit

--- src/test/java/BMICalculatorTests.java
+++ src/test/java/BMICalculatorTests.java
@@ -53,6 +53,24 @@ public class BMICalculatorTests {
 
         }
         @Test
+        public void checkOverweightCategoryHighLimit(){
+                System.setProperty("webdriver.chrome.driver","src\\test\\resources\\chromedriver.exe");
+                WebDriver driver = new ChromeDriver();
+                driver.get("https://healthunify.com/bmicalculator/");
+                WebElement weightInput = driver.findElement(By.name("wg"));
+                weightInput.sendKeys("81.68");
+                WebElement heightInput = driver.findElement(By.name("ht"));
+                heightInput.sendKeys("165");
+                WebElement calculateButton = driver.findElement(By.name("cc"));
+                calculateButton.click();
+                WebElement categoryInput = driver.findElement(By.name("desc"));
+                String actualCategory = categoryInput.getAttribute("value");
+
+                assertEquals(actualCategory,"Your category is Overweight","Category should be Overweight");
+                driver.quit();
+
+        }
+        @Test
         public void checkStarvationCategoryBoundaryValue(){
                 System.setProperty("webdriver.chrome.driver","src\\test\\resources\\chromedriver.exe");
                 WebDriver driver = new ChromeDriver();
