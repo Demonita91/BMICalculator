e26932a271ad3d20c78efffc1f2a3891580c47dc
asus <de>
2020-09-18 16:01:10 +0300

    Fixed margins, spaces, slashes

--- src/test/java/BMICalculatorTests.java
+++ src/test/java/BMICalculatorTests.java
@@ -7,106 +7,109 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 public class BMICalculatorTests {
-        @Test
-        public void checkOverweightCategory(){
-                //Установить системную переменную
-                System.setProperty("webdriver.chrome.driver","src\\test\\resources\\chromedriver.exe");
+    @Test
+    public void checkOverweightCategory() {
+        //Установить системную переменную
+        System.setProperty("webdriver.chrome.driver", "src//test//resources//chromedriver.exe");
 
-                //Открыть браузер
-                WebDriver driver = new ChromeDriver();
-                //Зайти на сайт https://healthunify.com/bmicalculator/
-                driver.get("https://healthunify.com/bmicalculator/");
-                //Ввести вес
-                WebElement weightInput = driver.findElement(By.name("wg"));
-                weightInput.sendKeys("73");
-                //Ввести рост
-                WebElement heightInput = driver.findElement(By.name("ht"));
-                heightInput.sendKeys("165");
+        //Открыть браузер
+        WebDriver driver = new ChromeDriver();
+        //Зайти на сайт https://healthunify.com/bmicalculator/
+        driver.get("https://healthunify.com/bmicalculator/");
+        //Ввести вес
+        WebElement weightInput = driver.findElement(By.name("wg"));
+        weightInput.sendKeys("73");
+        //Ввести рост
+        WebElement heightInput = driver.findElement(By.name("ht"));
+        heightInput.sendKeys("165");
 
-                //Нажать кнопку Calculate
-                WebElement calculateButton = driver.findElement(By.name("cc"));
-                calculateButton.click();
-                //Проверить, что категория Overweight
-                WebElement categoryInput = driver.findElement(By.name("desc"));
-                String actualCategory = categoryInput.getAttribute("value");
+        //Нажать кнопку Calculate
+        WebElement calculateButton = driver.findElement(By.name("cc"));
+        calculateButton.click();
+        //Проверить, что категория Overweight
+        WebElement categoryInput = driver.findElement(By.name("desc"));
+        String actualCategory = categoryInput.getAttribute("value");
 
-                assertEquals(actualCategory, "Your category is Overweight","Category should be Overweight");
-                driver.quit();
+        assertEquals(actualCategory, "Your category is Overweight", "Category should be Overweight");
+        driver.quit();
 
-        }
-        @Test
-        public void checkOverweightCategoryLowerLimit(){
-                System.setProperty("webdriver.chrome.driver","src\\test\\resources\\chromedriver.exe");
-                WebDriver driver = new ChromeDriver();
-                driver.get("https://healthunify.com/bmicalculator/");
-                WebElement weightInput = driver.findElement(By.name("wg"));
-                weightInput.sendKeys("68.1");
-                WebElement heightInput = driver.findElement(By.name("ht"));
-                heightInput.sendKeys("165");
-                WebElement calculateButton = driver.findElement(By.name("cc"));
-                calculateButton.click();
-                WebElement categoryInput = driver.findElement(By.name("desc"));
-                String actualCategory = categoryInput.getAttribute("value");
+    }
 
-                assertEquals(actualCategory,"Your category is Overweight","Category should be Overweight");
-                driver.quit();
+    @Test
+    public void checkOverweightCategoryLowerLimit() {
+        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://healthunify.com/bmicalculator/");
+        WebElement weightInput = driver.findElement(By.name("wg"));
+        weightInput.sendKeys("68.1");
+        WebElement heightInput = driver.findElement(By.name("ht"));
+        heightInput.sendKeys("165");
+        WebElement calculateButton = driver.findElement(By.name("cc"));
+        calculateButton.click();
+        WebElement categoryInput = driver.findElement(By.name("desc"));
+        String actualCategory = categoryInput.getAttribute("value");
 
-        }
-        @Test
-        public void checkOverweightCategoryHighLimit(){
-                System.setProperty("webdriver.chrome.driver","src\\test\\resources\\chromedriver.exe");
-                WebDriver driver = new ChromeDriver();
-                driver.get("https://healthunify.com/bmicalculator/");
-                WebElement weightInput = driver.findElement(By.name("wg"));
-                weightInput.sendKeys("81.68");
-                WebElement heightInput = driver.findElement(By.name("ht"));
-                heightInput.sendKeys("165");
-                WebElement calculateButton = driver.findElement(By.name("cc"));
-                calculateButton.click();
-                WebElement categoryInput = driver.findElement(By.name("desc"));
-                String actualCategory = categoryInput.getAttribute("value");
+        assertEquals(actualCategory, "Your category is Overweight", "Category should be Overweight");
+        driver.quit();
 
-                assertEquals(actualCategory,"Your category is Overweight","Category should be Overweight");
-                driver.quit();
+    }
 
-        }
-        @Test
-        public void checkStarvationCategoryBoundaryValue(){
-                System.setProperty("webdriver.chrome.driver","src\\test\\resources\\chromedriver.exe");
-                WebDriver driver = new ChromeDriver();
-                driver.get("https://healthunify.com/bmicalculator/");
-                WebElement weightInput = driver.findElement(By.name("wg"));
-                weightInput.sendKeys("44.9");
-                WebElement heightInput = driver.findElement(By.name("ht"));
-                heightInput.sendKeys("173");
-                WebElement calculateButton = driver.findElement(By.name("cc"));
-                calculateButton.click();
-                WebElement categoryInput = driver.findElement(By.name("desc"));
-                String actualCategory = categoryInput.getAttribute("value");
+    @Test
+    public void checkOverweightCategoryHighLimit() {
+        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://healthunify.com/bmicalculator/");
+        WebElement weightInput = driver.findElement(By.name("wg"));
+        weightInput.sendKeys("81.68");
+        WebElement heightInput = driver.findElement(By.name("ht"));
+        heightInput.sendKeys("165");
+        WebElement calculateButton = driver.findElement(By.name("cc"));
+        calculateButton.click();
+        WebElement categoryInput = driver.findElement(By.name("desc"));
+        String actualCategory = categoryInput.getAttribute("value");
 
-                assertEquals(actualCategory,"Your category is Starvation","Category should be Starvation");
-                driver.quit();
+        assertEquals(actualCategory, "Your category is Overweight", "Category should be Overweight");
+        driver.quit();
 
-        }
-        @Test
-        public void checkStarvationCategory(){
-                System.setProperty("webdriver.chrome.driver","src\\test\\resources\\chromedriver.exe");
-                WebDriver driver = new ChromeDriver();
-                driver.get("https://healthunify.com/bmicalculator/");
-                WebElement weightInput = driver.findElement(By.name("wg"));
-                weightInput.sendKeys("43");
-                WebElement heightInput = driver.findElement(By.name("ht"));
-                heightInput.sendKeys("173");
-                WebElement calculateButton = driver.findElement(By.name("cc"));
-                calculateButton.click();
-                WebElement categoryInput = driver.findElement(By.name("desc"));
-                String actualCategory = categoryInput.getAttribute("value");
+    }
 
-                assertEquals(actualCategory,"Your category is Starvation","Category should be Starvation");
-                driver.quit();
+    @Test
+    public void checkStarvationCategoryBoundaryValue() {
+        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://healthunify.com/bmicalculator/");
+        WebElement weightInput = driver.findElement(By.name("wg"));
+        weightInput.sendKeys("44.9");
+        WebElement heightInput = driver.findElement(By.name("ht"));
+        heightInput.sendKeys("173");
+        WebElement calculateButton = driver.findElement(By.name("cc"));
+        calculateButton.click();
+        WebElement categoryInput = driver.findElement(By.name("desc"));
+        String actualCategory = categoryInput.getAttribute("value");
 
-        }
+        assertEquals(actualCategory, "Your category is Starvation", "Category should be Starvation");
+        driver.quit();
 
+    }
+
+    @Test
+    public void checkStarvationCategory() {
+        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://healthunify.com/bmicalculator/");
+        WebElement weightInput = driver.findElement(By.name("wg"));
+        weightInput.sendKeys("43");
+        WebElement heightInput = driver.findElement(By.name("ht"));
+        heightInput.sendKeys("173");
+        WebElement calculateButton = driver.findElement(By.name("cc"));
+        calculateButton.click();
+        WebElement categoryInput = driver.findElement(By.name("desc"));
+        String actualCategory = categoryInput.getAttribute("value");
+
+        assertEquals(actualCategory, "Your category is Starvation", "Category should be Starvation");
+        driver.quit();
+
+    }
 
 
 }
